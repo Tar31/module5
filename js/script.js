c0dae8d73988297081ae601e79b8c33a2828c33e
Tar31 <84608140+Tar31@users.noreply.github.com>
2021-06-30 18:03:39 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ function buildAndShowHomeHTML (categories) {
       
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name; 
       
-        var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml, 'randomCategoryShortName', '\''+chosenCategoryShortName+'\''); 
+        var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml, "randomCategoryShortName", "'"+chosenCategoryShortName+"'"); 
                   insertHtml("#main-content", homeHtmlToInsertIntoMainPage);          
       
 
